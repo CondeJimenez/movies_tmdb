0c27fbdd3471cc6e0869edda6b93ba598b1b7487
EugeniaRS <kenayanrkt@gmail.com>
2023-04-17 21:22:35 -0600

    analisis(questions):look-quest

--- src/modules/Movies/MoviesPresentacional.jsx
+++ src/modules/Movies/MoviesPresentacional.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 
+// variable que instancia la api de IMGs
 const BASE_IMG = import.meta.env.VITE_API_URL_IMG;
+//porque meta.env
 const MoviesPresentacional = ({ peliculas }) => {
+  // se trai la prop peliculas ya destrucutrada 
   return (
     <section>
+       {/* entonces es por defecto que train la ruta? osea APi popular, top_reted, now_playing */}
       {/* en el link to hay que poner la direccion a la que sera el detalle de la pelicula */}
       <Link>
         <h1>{peliculas.title}</h1>
       </Link>
+      {/* porque se utilizan los ` `  */}
+      {/* no entiendo esta sintaxis `${variable que trai las imagenes de la Api} ${aqui pues extrae la imagen de la API}` */}
       <img
         src={`${BASE_IMG}${peliculas.poster_path}`}
         alt="Poster extraido de la API de TMDB"
